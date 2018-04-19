6599b711d7767a1369c331f60c4b509ec7d6b583
Tommy Howell <tommyhowell6@gmail.com>
2018-04-18 22:50:00 -0600

    updated script

--- ncbi-blast-2.7.1+/bin/blastScript.sh
+++ ncbi-blast-2.7.1+/bin/blastScript.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ./makeblastdb -in Relative.fasta -parse_seqids -dbtype prot
-./blastp -query /Users/tommyhowell/School/Bio465/Protein-Function-Prediction/target.237561.fasta -db Relative.fasta -outfmt 6 -task blastp -out output23.txt
+./blastp -query target.237561.fasta -db Relative.fasta -outfmt 6 -task blastp -out outputCandida.txt
+#requires Biopython to be installed
+python nullDistribution
+./blastp -query randomSeqs.fasta -db Relative.fasta -outfmt 6 -task blastp -out outputRand2.txt
